b1ca81793048eab85cdd221471f77da18b161dd1
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2021-12-12 14:52:42 +0100

    Add a packaging test

---
+++ test/packaging.test.ts
@@ -0,0 +1,20 @@
+import { execSync } from 'child_process';
+
+test('NPM package content', () => {
+    const expectedFiles = [ 
+        /^LICENSE$/, 
+        /^README\.md$/, 
+        /^package\.json$/, 
+        /^dist\/index\.(esm|cjs|min)\.(js(\.map)?|d\.ts)$/,
+        /^dist\/zbar\.wasm(\.bin)?$/,
+    ]
+
+    const pkg = JSON.parse(execSync('npm pack --dry-run --json').toString())[0]
+
+    expect(pkg.name).toEqual('zbar.wasm')
+    
+    expect(pkg.files.length).toBeGreaterThanOrEqual(13)
+
+    const extraFiles = pkg.files.map(obj => obj.path).filter(p => !expectedFiles.some(e => e.test(p)))
+    expect(extraFiles).toEqual([])
+})
